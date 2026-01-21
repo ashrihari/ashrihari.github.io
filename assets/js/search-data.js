ffd3dafaffa1f2612581c93098a4a2740019b572
ashrihari <41783468+ashrihari@users.noreply.github.com>
2026-01-21 01:20:04 +0000

    Deploying to gh-pages from @ ashrihari/ashrihari.github.io@1ec194978ec39eefe805b9f9a1f7ef5b93a36c57 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -439,9 +439,9 @@ ninja.data = [{
             },},{id: "news-a-simple-inline-announcement-with-markdown-emoji-sparkles-smile",
           title: 'A simple inline announcement with Markdown emoji! :sparkles: :smile:',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-snaak-smart-nutritional-automated-assembly-kiosk",
+          title: 'SNAAK - Smart Nutritional Automated Assembly Kiosk',
+          description: "",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
             },},{id: "projects-project-2",
